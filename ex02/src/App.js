8cfd7ef04b2fd1caa34eac494e0302564ac1596e
Meša Selimović <mesa_sel@hotmail.com>
2021-03-31 02:19:19 +0200

    This was useful, Day23!

---
+++ ex02/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React, { Component } from "react";
+import Plant from "./Plant";
+
+function App() {
+  return (
+    <div className="App">
+      <Plant />
+    </div>
+  );
+}
+
+export default App;
